4dd8736d3c45bf71384a7d10db9804c57cffc803
paollaalvesgeraldo1 <paollaalvesgeraldo1@gmail.com>
2025-09-18 21:11:29 -0300

    Add files via upload

---
+++ Agendamento.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=Agendamento.d.ts.map
---
+++ Agendamento.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const prompt = require("prompt-sync")();
+let agenda = [];
+const agendamentosComInterface = function agendarConsulta(agendamentos, []) {
+    agenda.push({ ID: id, nome: paciente, Data: data });
+    console.log(`Consulta agendada para ${data} em nome de ${paciente}`);
+};
+function cancelarConsulta(agendamentos, []) {
+    let index = agendamentosComInterface.indexOf(agenda);
+}
+function listarConsultas() {
+    console.log(`Agendamentos`);
+    console.log(`Paciente: ${nome} dia ${data}`);
+}
+console.log(`Agendamento médico`);
+function menuPrincipal() {
+    while (true) {
+        console.log(`Menu Principal`);
+        console.log(`1.Agendar consulta`);
+        console.log(`2.Listar consultas`);
+        console.log(`3.Cancelar consulta`);
+        let opcao = prompt(`Escolha uma opçao`);
+        if (opcao === '1') {
+            let opcaoAgendarConsulta = prompt(`Digite o ID do paciente`);
+            const nome = parseFloat(prompt(`Ǹome: `));
+            const data = parseFloat(prompt(`Data: `));
+        }
+        else if (opcao === '2') {
+            listarConsultas();
+        }
+        else if (opcao === '3') {
+            cancelarConculta();
+        }
+    }
+}
+menuPrincipal();
+//# sourceMappingURL=Agendamento.js.map
